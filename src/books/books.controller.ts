a454e70db49067d432ae075d3b9326e32c6ebfa2
shanmugapriyan98 <sravic23@asu.edu>
2024-06-19 21:54:32 -0500

    Added dtos and endpoints

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { CreateBookDTO } from './dto/create-book.dto';
+import { UpdateBookDTO } from './dto/update-book.dt';
 
 @Controller('books')
 export class BooksController {
     constructor(private booksService: BooksService) {}
 
+    @Get(':id')
+    findOneBook(@Param('id') id: string){
+        return this.booksService.findBook(+id)
+    }
+
+    @Post()
+    createBook(@Body() body:CreateBookDTO){
+        return this.booksService.createBook(body)
+    }
+
     @Get()
     findAll(){
         return this.booksService.findBooks()
+    }
+
+    @Patch(':id')
+    updateBook(@Param('id') id:string, @Body() body: UpdateBookDTO){
+        return this.booksService.updateBook(+id, body)
     }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,11 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Book } from './entities/books.entity';
+import { CreateBookDTO } from './dto/create-book.dto';
+import { UpdateBookDTO } from './dto/update-book.dt';
 
 @Injectable()
 export class BooksService {
     private books: Book[] = []
 
+    createBook(data: CreateBookDTO): Book{
+        const book = {
+            id:1111,
+            ...data
+        };
+
+        this.books.push(book)
+        return book
+    }
+
+    findBook(id: number){
+        const searchedBook = this.books.find((book)=> book.id == id)
+        if(!searchedBook){
+            throw new NotFoundException(`Book with id ${id} not found`)
+        }
+        return searchedBook
+    }
+
     findBooks(){
         return this.books
+    }
+
+    updateBook(id: number, updateBook: UpdateBookDTO){
+        const book = this.findBook(id)
+        Object.assign(book, updateBook)
+        return book
     }
 }
